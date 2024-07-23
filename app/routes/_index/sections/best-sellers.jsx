f7530c6e161ea6de8c71e23b8b591e0d48cb9947
Matthew Charles Chan <57197723+matthew-charles-chan@users.noreply.github.com>
2024-07-23 11:37:10 -0700

    Homepage V1.1

--- app/routes/_index/sections/best-sellers.jsx
+++ app/routes/_index/sections/best-sellers.jsx
@@ -97,7 +97,7 @@ function CollectionHeading({
   return (
     <>
       <h2 className='text-3xl font-bold uppercase'>
-        TRENDING PRODUCTS
+        TRENDING PRODUCTS!
       </h2>
       <div>
         <Link
